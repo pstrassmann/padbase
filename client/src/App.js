bf43d1a1e1b062a4b0f41997ee00de2833f62915
pstrassmann <st32490@gmail.com>
2020-08-03 20:38:42 -0700

    Refactored styling to uses scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Provider } from 'react-redux';
 import AllDogs from './components/AllDogs';
 import store from './store';
