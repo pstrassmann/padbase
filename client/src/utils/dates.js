566a5c4e4450fb73bed788def4586bd2954c27b9
pstrassmann <st32490@gmail.com>
2020-08-31 10:19:37 -0700

    Updated date formatting and validation functions so that moment.js does not fall back on date()

--- client/src/utils/dates.js
+++ client/src/utils/dates.js
@@ -1,10 +1,26 @@
 import moment from 'moment';
+import { numbersOnly } from './text';
+
+const dateMask = (input) => {
+  return input.replace(/[^0-9-]/g, '').slice(0,8);
+}
 
 const formatDate = (date) => {
-  if (date === undefined) return 'N/A';
+  if (date === undefined || date === null) return 'N/A';
   return moment(date).format('MM-DD-YY');
-};
+}
 
+const validatedDate = (dateMMDDYY) => {
+  if (dateMMDDYY === undefined) return undefined;
+  const momentDate = moment(dateMMDDYY, 'MM-DD-YY').format();
+  if (momentDate === 'Invalid date') {
+    return null
+  } else {
+    return momentDate;
+  }
+}
+
+// Expects date object or properly formatted ISO Date e.g 2020-08-18
 const formatAge = (birthday) => {
   if (!birthday || birthday === 'Invalid date') return 'N/A';
   if (moment(birthday).isAfter(Date.now())) return 'Err';
@@ -19,4 +35,4 @@ const formatAge = (birthday) => {
   return `${Math.floor(monthAge / 12)}y ${monthAge % 12}m`;
 };
 
-export { formatDate, formatAge };
+export { dateMask, validatedDate, formatDate, formatAge };
