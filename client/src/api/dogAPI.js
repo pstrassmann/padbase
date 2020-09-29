212996630bfc12602b968bfc335471fbcbbd787e
pstrassmann <st32490@gmail.com>
2020-09-29 12:47:03 -0700

    Added deleteDog and addNewDogGroup fetch functions

--- client/src/api/dogAPI.js
+++ client/src/api/dogAPI.js
@@ -35,3 +35,41 @@ export const saveNewDog = async (dogItem) => {
     console.error(err);
   }
 };
+
+export const saveDogGroup = async (dogGroup) => {
+  try {
+    const response = await fetch('/api/dogs/group', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/JSON',
+      },
+      body: JSON.stringify(dogGroup),
+    });
+    const insertedDogs = await response.json();
+    if (insertedDogs.error) {
+      console.error(insertedDogs.error);
+    }
+    return insertedDogs;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const deleteDog = async (dogID) => {
+  try {
+    const response = await fetch('api/dogs', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/JSON',
+      },
+      body: JSON.stringify(dogID),
+    });
+    const deletedDog = await response.json();
+    if (deletedDog.error) {
+      console.error(deletedDog.error);
+    }
+    return deletedDog;
+  } catch (err) {
+    console.error(err);
+  }
+}
