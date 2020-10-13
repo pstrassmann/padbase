eea5db1e5e3c7fc1e7c456a07a8ee12e206f7f38
pstrassmann <st32490@gmail.com>
2020-10-13 12:22:41 -0700

    /auth/user now returns user object instead of just auth status

--- routes/auth.js
+++ routes/auth.js
@@ -15,16 +15,31 @@ router.get(
   passport.authenticate('google', {
     failureRedirect: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/',
     successRedirect: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/',
+    failureFlash: true,
   }),
-  // (req, res) => {
+  // // (req, res) => {
   //   res.redirect(process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000');
   // }
 );
 // @desc      Check if authenticated
-// @route     GET /auth/isAuthenticated
+// @route     GET /auth/user
 // @access    Public
-router.get('/isAuthenticated', (req, res) => {
-  res.json({isAuthenticated: req.isAuthenticated()})
+router.get('/user', (req, res) => {
+
+  const user = {
+    isAuthenticated: req.isAuthenticated(),
+    unauthorizedEmail: null,
+    firstName: req.user && req.user.firstName ? req.user.firstName : null,
+    email: req.user && req.user.email ? req.user.email : null,
+  }
+  // Get flash message added by Passport authentication if
+  // it exists. Message will just contain the email unauthorized email address
+  const flashObj = req.flash();
+  let unauthorizedEmail = null;
+  if (flashObj.error && flashObj.error.length > 0) {
+    unauthorizedEmail = flashObj.error[0];
+  }
+  res.json(user);
 })
 
 // @desc      Logout user
