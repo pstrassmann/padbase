07064bf3048bed14130cff34755f164a0d87f677
pstrassmann <st32490@gmail.com>
2020-10-15 10:03:29 -0700

    Added sign out button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
-
+const Navbar = ({ auth }) => {
   return (
     <nav className="navbar">
-    <span className="navbar__logo">PadBase</span>
+      <span className="navbar__logo">PadBase</span>
+      {auth.user.isAuthenticated && (
+        <a href="/auth/logout" className="navbar__logout">
+          <div className="navbar__logout__text">Sign out</div>
+          <FontAwesomeIcon icon={faSignOutAlt} />
+        </a>
+      )}
     </nav>
-    )
+  );
+};
 
-}
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-
-export default Navbar;
+export default connect(mapStateToProps, null)(Navbar);
