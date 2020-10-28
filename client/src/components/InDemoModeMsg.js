06515da89c24e4bd7c43bce48d89d8120a30dd6d
pstrassmann <st32490@gmail.com>
2020-10-27 18:06:30 -0700

    Added bell icon before msg

--- client/src/components/InDemoModeMsg.js
+++ client/src/components/InDemoModeMsg.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBell} from '@fortawesome/free-solid-svg-icons';
 
 const InDemoModeMsg = () => {
   return (
     <div className="in-demo-mode-msg-wrapper">
+      <FontAwesomeIcon icon={faBell} size='lg'/>
     <p className="in-demo-mode-msg">
       You are currently viewing PadBase in Demo Mode. All data and personal
       information has been generated randomly.
